1290ebbd30da3795c0169230350a41672f16840b
Alucard163 <romemix163@gmail.com>
2023-05-08 15:35:29 +0600

    add css extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,11 +1,23 @@
 import wepback from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/config";
 
-export function buildLoaders(): wepback.RuleSetRule[] {
+export function buildLoaders({isDev}: BuildOptions): wepback.RuleSetRule[] {
     const styleLoader = {
         test: /\.s[ac]ss$/i,
         use: [
-            "style-loader",
-            "css-loader",
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev ?
+                            '[path][name]__[local]'
+                            : '[hash:base64:8]',
+                    }
+                }
+            },
             "sass-loader",
         ]
     }
