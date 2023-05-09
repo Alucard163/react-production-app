7d1ee2866762fb085fed0ff07443f88dc1ea16c9
Alucard163 <romemix163@gmail.com>
2023-05-09 18:46:17 +0600

    toggle theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import React from "react";
-import {Counter} from "./components/Counter";
-import './index.scss';
+import React, {Suspense} from "react";
+import {Link, Route, Routes} from 'react-router-dom';
+
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+
+import {useTheme} from "./theme/useTheme";
+
+import './styles/index.scss';
 
 export const App = () => {
-  return (
-      <div className="app">
-          <Counter />
-      </div>
-  )
+    const {theme, toggleTheme} = useTheme();
+
+      return (
+          <div className={`app ${theme}`}>
+              <button onClick={toggleTheme}>Toggle</button>
+              <Link to={'/'}>Главная</Link>
+              <Link to={'/about'}>О сайте</Link>
+              <Suspense fallback={<div>...Loading</div>}>
+                  <Routes>
+                      <Route path={'/about'} element={<AboutPageAsync />}/>
+                      <Route path={'/'} element={<MainPageAsync />}/>
+                  </Routes>
+              </Suspense>
+          </div>
+      )
 }
